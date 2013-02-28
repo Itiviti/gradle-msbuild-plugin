e2fcb0d92ba0ef50266fae1570b46e261985322d
head-thrash <ilya.cherkasov@gmail.com>
2013-02-28 15:58:07 +0400

    Xbuild resolver implemented

--- src/main/groovy/com/ullink/MsbuildResolver.groovy
+++ src/main/groovy/com/ullink/MsbuildResolver.groovy
@@ -1,27 +1,24 @@
 package com.ullink
 
-import org.apache.commons.lang.NotImplementedException
+import org.gradle.api.GradleException
 
-interface IExecutableResolver {
-    void setupExecutable(Msbuild msbuild);
-}
 
 class MsbuildResolver implements IExecutableResolver {
     static final String MSBUILD_TOOLS_PATH = 'MSBuildToolsPath'
-    static final String MSBUILD_PREFIX         = "SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\"
+    static final String MSBUILD_PREFIX = "SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\"
     static final String MSBUILD_WOW6432_PREFIX = "SOFTWARE\\Wow6432Node\\Microsoft\\MSBuild\\ToolsVersions\\"
 
     void setupExecutable(Msbuild msbuild) {
         (msbuild.version == null ?
-            ["4.0", "3.5","2.0"] :
+            KNOWN_VERSIONS :
             [msbuild.version]).find { x ->
-                trySetMsbuild(msbuild, MSBUILD_WOW6432_PREFIX + x) ||
-                        trySetMsbuild(msbuild, MSBUILD_PREFIX + x)
+            trySetMsbuild(msbuild, MSBUILD_WOW6432_PREFIX + x) ||
+                trySetMsbuild(msbuild, MSBUILD_PREFIX + x)
         }
         msbuild.executable = 'msbuild.exe'
     }
 
-    boolean trySetMsbuild(Msbuild msbuild, String key) {
+    static boolean trySetMsbuild(Msbuild msbuild, String key) {
         def v = Registry.getValue(Registry.HKEY_LOCAL_MACHINE, key, MSBUILD_TOOLS_PATH)
         if (v != null && new File(v).isDirectory()) {
             msbuild.msbuildDir = v
@@ -31,12 +28,4 @@ class MsbuildResolver implements IExecutableResolver {
     }
 }
 
-class XbuildResolver implements IExecutableResolver {
-
-    void setupExecutable(Msbuild msbuild) {
-        msbuild.executable = 'xbuild'
-        throw new NotImplementedException()
-    }
-
-}
 
