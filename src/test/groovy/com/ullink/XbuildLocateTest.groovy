48e03e8c170f5ddbc5cc4b08e1f354714b41ab99
Timotei Dolean <timotei21@gmail.com>
2015-05-24 10:40:45 +0300

    Add proper support for Mac OS X and Linux
    Also, improve the detection logic for
    *nix-based OSes, in order to choose the latest
    xbuild version.

--- src/test/groovy/com/ullink/XbuildLocateTest.groovy
+++ src/test/groovy/com/ullink/XbuildLocateTest.groovy
@@ -1,24 +1,47 @@
 package com.ullink
 
+import org.gradle.api.GradleException
+import org.gradle.api.Project
 import org.gradle.internal.os.OperatingSystem
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 
 import static org.junit.Assert.assertNotNull
 
 class XbuildLocateTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void beforeMethod() {
-        org.junit.Assume.assumeTrue OperatingSystem.current().linux
+        org.junit.Assume.assumeTrue OperatingSystem.current().unix
         org.junit.Assume.assumeTrue "mono --version".execute().in.text != null
     }
 
     @Test
     public void testXBuildCanBeFound() {
         def resolver = new XbuildResolver()
-        def xbuildDir = resolver.getXBuildDir(null)
+        Project p = ProjectBuilder.builder().build()
+        p.apply plugin: 'msbuild'
+        def xbuildDir = resolver.getXBuildDir(p.tasks.msbuild)
         assertNotNull(xbuildDir)
     }
 
+    @Test
+    public void givenInvalidMSBuildVersion_throwsException() {
+        def resolver = new XbuildResolver()
+        Project p = ProjectBuilder.builder().build()
+        p.apply plugin: 'msbuild'
+        p.msbuild {
+            version = 999.3
+        }
+
+        expectedException.expect(GradleException.class)
+        expectedException.expectMessage("Cannot find an xbuild binary")
+        resolver.getXBuildDir(p.tasks.msbuild)
+
+    }
 }
