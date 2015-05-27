d2345fc9d193edb01554b45780911c5e638996b2
Marco Zwyssig <marco@zwyssig.info>
2015-05-27 15:00:16 +0200

    using : (instead =) for passing arguments to msbuild

--- src/main/groovy/com/ullink/Msbuild.groovy
+++ src/main/groovy/com/ullink/Msbuild.groovy
@@ -196,7 +196,7 @@ class Msbuild extends ConventionTask {
         def extMap = getExtensions()?.getExtraProperties()?.getProperties()
         if (extMap != null) {
             commandLineArgs += extMap.collect { k, v ->
-                v ? "/$k=$v" : "/$k"
+                v ? "/$k:$v" : "/$k"
             }
         }
 
