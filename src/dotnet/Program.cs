80c0f6d0f264fa8b809260ae61c2630fbe17bd9f
Francois Valdy <francois.valdy@gmail.com>
2015-06-03 12:11:10 +0200

    support item group specs

--- src/dotnet/Program.cs
+++ src/dotnet/Program.cs
@@ -34,15 +34,15 @@ namespace ProjectFileParser
 
         interface Evaluator
         {
-            IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project);
+            IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project, bool ignoreCondition);
             IEnumerable<Tuple<string, string>> GetEvaluatedMetadata(BuildItem proj);
         }
 
         class MonoEvaluator : Evaluator
         {
-            public IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project)
+            public IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project, bool ignoreCondition)
             {
-                var dic = (IDictionary<string, BuildItemGroup>)typeof(Project).GetProperty("EvaluatedItemsByName", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(project, new object[0]);
+                var dic = (IDictionary<string, BuildItemGroup>)typeof(Project).GetProperty(ignoreCondition ? "EvaluatedItemsByNameIgnoringCondition" : "EvaluatedItemsByName", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(project, new object[0]);
                 return dic.Select(e => Tuple.Create(e.Key, e.Value.Cast<BuildItem>()));
             }
 
@@ -55,9 +55,9 @@ namespace ProjectFileParser
 
         class MSBuildEvaluator : Evaluator
         {
-            public IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project)
+            public IEnumerable<Tuple<string, IEnumerable<BuildItem>>> GetEvaluatedItemsByName(Project project, bool ignoreCondition)
             {
-                return project.EvaluatedItems.Cast<BuildItem>().GroupBy(item => item.Name).Select(g => Tuple.Create(g.Key, g.Cast<BuildItem>()));
+                return (ignoreCondition ? project.EvaluatedItemsIgnoringCondition : project.EvaluatedItems).Cast<BuildItem>().GroupBy(item => item.Name).Select(g => Tuple.Create(g.Key, g.Cast<BuildItem>()));
             }
 
             public IEnumerable<Tuple<string, string>> GetEvaluatedMetadata(BuildItem proj)
@@ -70,7 +70,7 @@ namespace ProjectFileParser
 
         static IEnumerable<BuildItem> GetBuildLevelItems(Project project)
         {
-            return eval.GetEvaluatedItemsByName(project).Where(entry => entry.Item1.StartsWith("BuildLevel")).SelectMany(entry => entry.Item2);
+            return eval.GetEvaluatedItemsByName(project, false).Where(entry => entry.Item1.StartsWith("BuildLevel")).SelectMany(entry => entry.Item2);
         }
 
         static void Parse(String file)
@@ -109,16 +109,9 @@ namespace ProjectFileParser
         private static JObject ToJson(Project project)
         {
             JObject jProject = new JObject();
-            JObject properties = new JObject();
-            jProject["Properties"] = properties;
-            foreach (var entry in project.EvaluatedProperties.Cast<BuildProperty>())
-            {
-                properties[entry.Name] = entry.ToString();
-            }
-            foreach (var entry in eval.GetEvaluatedItemsByName(project))
+            foreach (var entry in eval.GetEvaluatedItemsByName(project, false))
             {
                 JArray items = new JArray();
-                jProject[entry.Item1] = items;
                 foreach (var item in entry.Item2)
                 {
                     var it = new JObject();
@@ -129,7 +122,35 @@ namespace ProjectFileParser
                     }
                     items.Add(it);
                 }
+                jProject[entry.Item1] = items;
             }
+            JObject properties = new JObject();
+            Func<BuildProperty, string> ToKey = s => "__" + s.Name + "__";
+
+            project.EvaluatedProperties
+                .Cast<BuildProperty>()
+                .Where(p => p.ToString().Trim().StartsWith("@"))
+                .ToList()
+                .Select(p =>
+                {
+                    if (RunningMono)
+                        project.AddNewItemGroup().AddNewItem(ToKey(p), p.ToString());
+                    else
+                        project.AddNewItem(ToKey(p), p.ToString());
+                    return p;
+                })
+                .ToList()
+                .ForEach(p =>
+                {
+                    var grp = eval.GetEvaluatedItemsByName(project, true).Where(t => t.Item1 == ToKey(p)).SelectMany(t => t.Item2);
+                    properties[p.Name] = string.Join(";", grp.Select(i => i.FinalItemSpec));
+                });
+            foreach (var entry in project.EvaluatedProperties.Cast<BuildProperty>().Where(p => !p.ToString().Trim().StartsWith("@")))
+            {
+                var value = entry.ToString();
+                properties[entry.Name] = entry.ToString();
+            }
+            jProject["Properties"] = properties;
             return jProject;
         }
     }
