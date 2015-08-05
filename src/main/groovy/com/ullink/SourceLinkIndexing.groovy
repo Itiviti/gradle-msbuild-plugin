a38262d54fdfef1fdf22a5cbc509fd71055101d1
Francois Valdy <francois.valdy@gmail.com>
2015-08-05 10:44:17 +0200

    Added support for pdbfile / sources args

--- src/main/groovy/com/ullink/SourceLinkIndexing.groovy
+++ src/main/groovy/com/ullink/SourceLinkIndexing.groovy
@@ -3,16 +3,20 @@ package com.ullink
 import org.gradle.api.internal.ConventionTask
 import org.gradle.api.tasks.TaskAction
 
+import java.lang.reflect.Array
+
 class SourceLinkIndexing extends ConventionTask {
     def projectFile
     def sourcelinkDir
     def properties = [:]
     def url
     def commit
+    def pdbFile
+    def sourceFiles
 
     SourceLinkIndexing() {
 
-        conventionMapping.map "projectFile", { project.tasks.msbuild.mainProject?.projectFile }
+        conventionMapping.map "projectFile", { pdbFile ? null : project.tasks.msbuild.mainProject?.projectFile }
         conventionMapping.map "properties", { project.tasks.msbuild.initProperties }
 
         project.afterEvaluate {
@@ -27,14 +31,27 @@ class SourceLinkIndexing extends ConventionTask {
     @TaskAction
     void run() {
         def args = [ "$sourcelinkDir/SourceLink.exe", 'index' ]
-        args += [ '-pr', getProjectFile() ]
+        if (getProjectFile()) args += [ '-pr', getProjectFile() ]
         if (commit) args += [ '-c', commit]
+        addArgs(args, '-p', pdbFile, { project.file(it) })
         if (url) args += [ '-u', url]
+        addArgs(args, '-f', sourceFiles, { it })
         getProperties().each {
             args += [ '-pp', it.key, it.value ]
         }
         project.exec {
             commandLine args
+        }
+    }
+
+    void addArgs(List args, def prefix, def value, def mapper) {
+        if (!value) return;
+        if (value instanceof Iterable || value instanceof Array) {
+            value.each {
+                args.addAll([prefix, mapper(it)])
+            }
+        } else {
+            args.addAll([prefix, mapper(value)])
         }
     }
 }
