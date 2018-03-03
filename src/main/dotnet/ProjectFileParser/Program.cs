fca07babe35ddda0434968b7a771652c6a7d85ea
Peter Ng <ngyukman@gmail.com>
2018-03-03 11:33:07 +0800

    Try another hack on mono

--- src/main/dotnet/ProjectFileParser/Program.cs
+++ src/main/dotnet/ProjectFileParser/Program.cs
@@ -3,40 +3,55 @@ using System.IO;
 using Microsoft.Build.Construction;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Reflection;
 
 namespace ProjectFileParser
 {
     class Program
     {
-        static int Main(string[] args)
+        // We have to set MSBUILD_EXE_PATH to some valid path for faking the BuildEnvironment on Mono
+        // or else will throw PlatformUnsupportedException
+        private class MonoHack : IDisposable
         {
-            SetIsMonoExplicitly();
-            try
+            private const string MSBUILD_EXE_PATH = "MSBUILD_EXE_PATH";
+            private readonly string _msbuildExePath;
+            private readonly bool _isMono;
+
+            public MonoHack()
             {
-                var obj = JObject.Parse(Console.In.ReadToEnd());
-                var result = Parse(args[0], obj);
-                Console.WriteLine(result.ToString());
+                _isMono = Type.GetType("Mono.Runtime") != null;
+                if (_isMono)
+                {
+                    _msbuildExePath = Environment.GetEnvironmentVariable(MSBUILD_EXE_PATH);
+                    Environment.SetEnvironmentVariable(MSBUILD_EXE_PATH, typeof(Program).Assembly.Location);
+                }
             }
-            catch (Exception e)
+
+            public void Dispose()
             {
-                Console.Error.WriteLine("Error during project file parsing: {0}", e);
-                return -1;
+                if (_isMono)
+                {
+                    Environment.SetEnvironmentVariable(MSBUILD_EXE_PATH, _msbuildExePath);
+                }
             }
-            return 0;
         }
 
-        // We have to set _isMono explicitly if running in Mono, otherwise we will have PlatformNotSupportedException when TryFromVisualStudioProcess
-        // _isMono can be found in https://github.com/Microsoft/msbuild/blob/master/src/Shared/NativeMethodsShared.cs
-        // TryFromVisualStudioProcess can be found in https://github.com/Microsoft/msbuild/blob/master/src/Shared/BuildEnvironmentHelper.cs
-        private static void SetIsMonoExplicitly()
+        static int Main(string[] args)
         {
-            if (Type.GetType("Mono.Runtime") != null)
+            using (new MonoHack())
             {
-                var nativeSharedMethod = typeof(SolutionFile).Assembly.GetType("Microsoft.Build.Shared.NativeMethodsShared");
-                var isMonoField = nativeSharedMethod.GetField("_isMono", BindingFlags.Static | BindingFlags.NonPublic);
-                isMonoField.SetValue(null, true);
+                try
+                {
+                    var obj = JObject.Parse(Console.In.ReadToEnd());
+                    var result = Parse(args[0], obj);
+                    Console.WriteLine(result.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Error during project file parsing: {0}", e);
+                    return -1;
+                }
             }
+            return 0;
         }
 
         static JObject Parse(string file, JObject args)
